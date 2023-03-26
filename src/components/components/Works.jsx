4579a2480d95b6ba09fb31401bdec3a1dd50b54f
enigma-devhub <emmanuelonoja291@gmail.com>
2023-03-26 13:30:34 +0100

    V5

--- src/components/components/Works.jsx
+++ src/components/components/Works.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Tilt from 'react-tilt';
+import Tilt from 'react-parallax-tilt';
 import { motion } from 'framer-motion';
 import { github } from '../../assets';
 import { SectionWrapper } from '../../hoc';
