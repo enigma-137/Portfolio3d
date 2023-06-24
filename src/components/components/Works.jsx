9538d2d92a4adae1004e81b9afbc092d4a2c3219
enigma-devhub <emmanuelonoja291@gmail.com>
2023-06-24 08:43:03 +0100

    jhfd

--- src/components/components/Works.jsx
+++ src/components/components/Works.jsx
@@ -7,7 +7,7 @@ import { projects } from '../../constants';
 import { fadeIn, textVariant } from '../../utils/motion';
 import { styles } from '../../styles';
 
-
+//project card
 const ProjectCard =({index, name, description, tags, image, source_code_link})=>{
   return(
   <motion.div variants={fadeIn("up", "spring", index * 0.5, 1)}>
